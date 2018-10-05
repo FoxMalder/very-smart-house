5c39661bae44d1c3d7226af2c071488e4d3fbf16
preigile <diva.artim@gmail.com>
2018-10-05 11:34:03 +0300

    create component for card

--- app/components/Cards.js
+++ app/components/Cards.js
@@ -1,3 +1,14 @@
+import cardEvents from '../../mock/events';
+
+import Card from "./Card";
+
 export default () => {
-    return `<div class="cards"> </div>`
+    console.log('events', cardEvents);
+
+    return `<div class="cards">
+                <div class="cards__title">Лента событий </div>
+                <div class="cards__container">
+                    ${cardEvents.events.map(Card).join('')}
+                </div>
+            </div>`
 }
