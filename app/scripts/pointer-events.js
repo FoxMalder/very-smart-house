1d3aad4f24189060316b94045d9a34f310f581f4
preigile <diva.artim@gmail.com>
2018-10-07 15:39:51 +0300

    image resize

--- app/scripts/pointer-events.js
+++ app/scripts/pointer-events.js
@@ -1,53 +1,70 @@
 export default () => {
     let camera = document.getElementById('camera');
     let currentGesture = null;
+    let eventsCache = [];
+    let prevDiff = -1;
 
     const nodeState = {
         scale: 1,
         translate: 0,
         brightness: 100,
-        startPosition: 0,
     };
 
-    camera.addEventListener('pointerdown', (event) => {
-        // Нужно для десктопа чтобы поймать pointerup вне DOM-ноды
-        camera.setPointerCapture(event.pointerId);
+    if ("ontouchstart" in document.documentElement) {
+        camera.addEventListener('pointerdown', (event) => {
+            eventsCache.push(event);
 
-        currentGesture = {
-            startX: event.x,
-            prevX: event.x,
-            prevTs: Date.now(),
-            scale: nodeState.scale,
-            translate: nodeState.translate,
-            brightness: nodeState.brightness,
-            startPosition: nodeState.startPosition,
-        };
-    });
+            camera.setPointerCapture(event.pointerId);
 
-    camera.addEventListener('pointermove', (event) => {
-        if (!currentGesture) {
-            return
-        }
+            currentGesture = {
+                startX: event.x,
+                prevX: event.x,
+                scale: nodeState.scale,
+                translate: nodeState.translate,
+                brightness: nodeState.brightness,
+            };
+        });
 
-        const {startX, prevX, prevTs, scale, translate, brightness, startPosition} = currentGesture;
-        const {x} = event;
-        const dx = x - startX;
+        camera.addEventListener('pointermove', (event) => {
+            if (!currentGesture) {
+                return;
+            }
 
-        camera.style.transform = `translate(${translate + dx}px)`;
+            for (let i = 0; i < eventsCache.length; i++) {
+                if (event.pointerId === eventsCache[i].pointerId) {
+                    eventsCache[i] = event;
+                    break;
+                }
+            }
 
-        // const ts = Date.now();
-        // const speed = Math.abs(x - prevX) / (ts - prevTs);
+            if (eventsCache.length === 1) {
+                const {startX, prevX, scale, translate, brightness} = currentGesture;
+                const {x} = event;
+                const dx = x - startX;
 
-        // if (speed > 5 && ts !== prevTs) {
-        //     // container.className = `${container.className} notification-container_moved`;
-        //     camera.className = `${camera.className} notification-container__camera_moved-${dx > 0 ? 'right' : 'left'}`;
-        //     currentGesture = null;
-        //     return;
-        // }
+                camera.style.transform = `translate(${translate + dx}px)`;
 
-        currentGesture.prevX = x;
-        // currentGesture.prevTs = ts;
-        nodeState.translate = dx < -1.5 * nodeState.translate ? -2 * nodeState.translate : 0;
-    });
+                currentGesture.prevX = x;
+                nodeState.translate = dx < -1.5 * nodeState.translate ? -2 * nodeState.translate : 0;
+            }
 
+            if (eventsCache.length === 2) {
+                let curDiff = Math.abs(eventsCache[0].clientX - eventsCache[1].clientX);
+                let size = document.getElementById('camera__oncoming');
+
+                if (prevDiff > 0) {
+                    if (curDiff > prevDiff) {
+                        size.innerText = `${curDiff}`;
+                        camera.style.transform = `scale(${1 + curDiff * 0.1})`;
+                    }
+                    if (curDiff < prevDiff) {
+                        size.innerText = `${curDiff}`;
+                        camera.style.transform = `scale(${1 - curDiff * 0.1})`;
+                    }
+                }
+
+                prevDiff = curDiff;
+            }
+        });
+    }
 }
